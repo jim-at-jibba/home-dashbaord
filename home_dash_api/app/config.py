b974714d26bc90a853837e2b019dbb6dae8dd090
James Best <jim@justjibba.net>
2022-08-06 05:18:33 +0100

    Add caching to get_settings

--- home_dash_api/app/config.py
+++ home_dash_api/app/config.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from functools import lru_cache
 
 from pydantic import BaseSettings
 
@@ -11,6 +12,7 @@ class Settings(BaseSettings):
     testing: bool = os.getenv("TESTING", False)
 
 
+@lru_cache()
 def get_settings() -> BaseSettings:
     log.info("Getting settings")
     return Settings()
